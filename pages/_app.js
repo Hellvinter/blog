c91cedbb4ae666156a4c4a9c5a66ce5718e04e1d
Hellvinter <hellvintertyr@gmail.com>
2020-04-27 18:37:34 +0300

    deleted unnecessery elements

---
+++ pages/_app.js
@@ -0,0 +1,6 @@
+//import normalize and global css styles
+import "../styles/global_styles.css";
+
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
